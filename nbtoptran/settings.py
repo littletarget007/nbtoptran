abe584f1cd1197e0dc483feda4bb6e7ee488c9ac
littletarget007 <1500483510@qq.com>
2018-06-26 14:53:22 +0800

    Update settings.py

--- nbtoptran/settings.py
+++ nbtoptran/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#!+zq@^b4c_=q#t0@v71g*qa&8(_uwvy7x1e(vz!b0x+j10j9i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Fasle
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','.nbtoptran.com']
 
