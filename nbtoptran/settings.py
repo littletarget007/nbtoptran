a7a0053d223bbff7d5eebce8b0abaac8e0fdc3e7
littletarget007 <1500483510@qq.com>
2018-07-04 18:39:43 +0800

    Update settings.py

--- nbtoptran/settings.py
+++ nbtoptran/settings.py
@@ -15,7 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import sys,io
 
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
