e4651d1f23b43a5a4f8229ecd52811d9ed208b35
littletarget007 <1500483510@qq.com>
2018-07-08 19:58:00 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 
-import sys,io
+import sys
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='utf-8')
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nbtoptran.settings")
     try:
